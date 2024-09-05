5e9928ad3c79b95b1128d723f1a77261a04e07d7
Baas Zunnaiyyer <baaszunnaiyyer@gmail.com>
2024-09-05 11:55:12 +0500

    Middleware Basics

--- app.js
+++ app.js
@@ -1,63 +1,42 @@
 const express = require('express')
 const app = express()
-const {products} = require('./data.js')
+const logger = require('./logger')
+const authorize = require('./authorize')
+const morgan = require('morgan')
+
+// REQ => MIDDLE WARE => RES
+app.use('/api/items',[authorize,logger])
+
+//app.use is used for applying middle ware in all the routes
+
+//app.use('/api', logger) will apply middle ware in all the requested URl that are Related to api i.e /api/product, /api/Items
+
+//1. use vs route
+//2. options - our own /express/ third party
+
+// setup static and middleware
+app.use(express.static('./public'))
+
+app.use(morgan('tiny'))
 
 
-
-app.get('/',(req,res)=>{
-    res.send('<h1>Home Page</h1><a href="/api/products"> products</a>')
-})
-
-app.get('/api/products',(req,res)=>{
-    const newProduct = products.map((product)=>{
-        const {id,name,image} = product
-        return {id, name, image}
-    })
-
-    res.json(newProduct)
-})
-
-app.get('/api/products/:productsID',(req,res)=>{
-    // console.log(req)
-    // console.log(req.params)
-
-    const {productsID} = req.params
-
-    const singleProduct = products.find((product)=> product.id == Number(productsID))
+app.get('/', (req,res)=>{
     
-    if(!singleProduct){
-        return res.status(404).send('Product Not Found')
-    }
-    return res.json(singleProduct)
+
+    res.send('Home')
 })
 
-app.get('/api/products/:productID/review/:reviewID', (req,res)=>{
-    console.log(req.params);
-    res.send('Hello World')
-    
+app.get('/about', (req,res)=>{
+    res.send('about')
 })
 
-app.get('/api/huhu/bubu', (req,res)=>{
+app.get('/api/products', (req,res)=>{
+    res.send('Products')
+})
 
-    console.log(req.query);
-    const {search,limit} = req.query
-    let sortedproduct = [...products]
-
-    if(search){
-        sortedproduct = sortedproduct.filter((product)=>{
-            return product.name.startsWith(search)
-        })
-    }
-    if(limit){
-        sortedproduct = sortedproduct.slice(0,Number(limit))
-    }
-    if(sortedproduct.length < 1){
-        // res.status(200).send('product not found')
-        return res.status(200).json({sucess : true, data : []})
-    }
-    return res.status(200).json(sortedproduct)
-    // return here is not mendadtory
-    
+app.get('/api/items', (req,res)=>{
+    console.log(req.user)
+    res.send('Items')
 })
 
 app.listen(5000, ()=>{
