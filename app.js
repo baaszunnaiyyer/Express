d3076865efeb0e0fcb88488e0d8aed7b70698949
Baas Zunnaiyyer <baaszunnaiyyer@gmail.com>
2024-09-07 23:06:00 +0500

    Get and Push Metod

--- app.js
+++ app.js
@@ -1,42 +1,22 @@
 const express = require('express')
 const app = express()
-const logger = require('./logger')
-const authorize = require('./authorize')
-const morgan = require('morgan')
+const {people} = require('./data')
 
-// REQ => MIDDLE WARE => RES
-app.use('/api/items',[authorize,logger])
+//static Assest
+app.use(express.static('./methods-public'))
 
-//app.use is used for applying middle ware in all the routes
+app.use(express.urlencoded({extended:false}))
 
-//app.use('/api', logger) will apply middle ware in all the requested URl that are Related to api i.e /api/product, /api/Items
-
-//1. use vs route
-//2. options - our own /express/ third party
-
-// setup static and middleware
-app.use(express.static('./public'))
-
-app.use(morgan('tiny'))
-
-
-app.get('/', (req,res)=>{
-    
-
-    res.send('Home')
+app.get('/api/people',(req,res)=>{
+    res.status(200).json({sucess:true,data:people})
 })
 
-app.get('/about', (req,res)=>{
-    res.send('about')
-})
-
-app.get('/api/products', (req,res)=>{
-    res.send('Products')
-})
-
-app.get('/api/items', (req,res)=>{
-    console.log(req.user)
-    res.send('Items')
+app.post('/login',(req,res)=>{
+    const {Testing} = req.body;
+    if(Testing){
+        return res.status(200).send(`Welcome ${Testing}`)
+    }    
+    res.status(401).send("please Provide Credintials")
 })
 
 app.listen(5000, ()=>{
